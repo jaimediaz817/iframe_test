8a0b3ba4365efc7943c1792db226b82c22ea4fee
= <=>
2021-09-21 10:44:31 -0500

    ajustes iframe add

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!-- <?php
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-    $ch = curl_init();
-    $timeout = 5;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    echo $data;
-}
-?> -->
-
 <!DOCTYPE html>
 <html>
   <head>
