87d68b7f4469a0a6f3cb8ce216e9712280379729
= <=>
2021-09-21 16:10:42 -0500

    changes

--- index.php
+++ index.php
@@ -416,7 +416,7 @@
 			</section>
 
 			<section id="block4" class="module-block">				
-				<h2>Opción #4 inrustación a través de formulario</h2>
+				<h2>Opción #4 inrustación a través de Iframe y consultas desde el mismo HTML origen</h2>
                 <div>
                     <iframe src="https://www.kayak.com/horizon/sem/flights/region/r129?lang=en&skipapp=true" width="675" height="410" frameborder="0" marginheight="0" marginwidth="0"></iframe>
                         <br />
