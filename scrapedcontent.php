827cb5e696bbd2ec2ef2465f9a0344d9d9560405
= <=>
2021-09-21 17:18:02 -0500

    changes

--- scrapedcontent.php
+++ scrapedcontent.php
@@ -1,4 +1,11 @@
 <?php
+    $script1 = file_get_contents('https://static.flightstats.com/v2/prod/c-11864f-467/manifest-d6d37b619cff0b6ce5ac.js');
+    $script2 = file_get_contents('https://static.flightstats.com/v2/prod/c-11864f-467/vendor-d6d37b619cff0b6ce5ac.js');
+    $script3 = file_get_contents('https://static.flightstats.com/v2/prod/c-11864f-467/main-d6d37b619cff0b6ce5ac.js');
     $homepage = file_get_contents('https://www.flightstats.com/v2');
+
+    echo $script1;
+    echo $script2;
+    echo $script3;
     echo $homepage;
 ?>
